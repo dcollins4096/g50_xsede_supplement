150322e53719391df64a959a3f80c136c93f4d94
dcollins <dccollins@fsu.edu>
2023-02-01 16:05:36 -0500

    Draft 1.

---
+++ plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pdb
+nar=np.array
+
+f1 = open('sigma_table_256.txt','r')
+lines = f1.readlines()
+f1.close()
+mach256=[]
+sigma256=[]
+for line in lines:
+    m,s=nar(line.split('\t'),dtype='float')
+    mach256.append(m)
+    sigma256.append(s)
+mach256=nar(mach256)
+f2 = open('sigma_table_1024.txt','r')
+lines = f2.readlines()
+f2.close()
+mach1024=[]
+sigma1024=[]
+for line in lines:
+    m,s=nar(line.split('\t'),dtype='float')
+    mach1024.append(m)
+    sigma1024.append(s)
+
+plt.clf()
+plt.scatter( mach256,sigma256, c='k')
+pfit=np.polyfit(mach256[:3],sigma256[:3],1)
+plt.plot( mach256, pfit[0]*mach256+pfit[1],c='k')
+plt.scatter( mach1024,sigma1024, c='r')
+plt.xlim(0,8.2)
+plt.ylim(0,12)
+plt.xlabel('Mach')
+plt.ylabel(r'$\sigma_\rho$')
+#plt.plot( mach256,sigma256,marker='*')
+#plt.plot( mach1024,sigma1024,marker='*')
+plt.savefig('resolution.png')
+
+print(lines)
